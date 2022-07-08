e30f2de1e3b01aa1ec84d9456c8d40448479c9dc
JViniX <47263194+JViniX@users.noreply.github.com>
2022-07-07 20:26:59 -0400

    Week08 - Authentication

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,32 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 let inventoryController = require('../controllers/inventory');
-
-// Connect to our model
-let Inventory = require('../models/inventory');
-
+let authController = require('../controllers/auth');
 
 // helper function for guard purposes
-function requireAuth(req, res, next)
-{
-    // check if the user is logged in
-    if(!req.isAuthenticated())
-    {
-        req.session.url = req.originalUrl;
-        return res.redirect('/users/signin');
-    }
-    next();
-}
+// function requireAuth(req, res, next)
+// {
+//     // check if the user is logged in
+//     if(!req.isAuthenticated())
+//     {
+//         req.session.url = req.originalUrl;
+//         return res.redirect('/users/signin');
+//     }
+//     next();
+// }
 
 /* GET list of items */
 router.get('/list', inventoryController.inventoryList);
 
 // Routers for edit
 // router.get('/edit/:id', requireAuth, inventoryController.displayEditPage);
-router.put('/edit/:id', inventoryController.processEdit);
+router.put('/edit/:id', authController.requireAuth, inventoryController.processEdit);
 
 // Delete
-router.delete('/delete/:id', inventoryController.performDelete);
+router.delete('/delete/:id', authController.requireAuth, inventoryController.performDelete);
 
 
 /* GET Route for displaying the Add page - CREATE Operation */
